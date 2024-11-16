850cedef04a15232d37c98c9084728d994d33a87
biooids <biooidsbiooid@gmail.com>
2024-11-16 07:20:04 +0200

    "Refactored Pricing component to display two payment options with updated layout and design"

--- frontend/src/components/pages/pay/Pricing.tsx
+++ frontend/src/components/pages/pay/Pricing.tsx
@@ -89,64 +89,80 @@ function Pricing() {
     onClose: () => {},
   };
   return (
-    <section className="min-h-screen ">
-      <div className="pay-card p-3 rounded-lg max-w-[300px] flex flex-col gap-5 m-auto mt-5">
-        <h2 className="text-3xl font-bold">Pro</h2>
-        <p>
-          Kanda ahakurikira maze w'ishyure, twashyize ku{" "}
-          <span className="font-bold text-green-500">1300 Rwf.</span> Ufite
-          ikibazo twandikire kuri{" "}
-          <span className="font-bold text-green-500">WhatsApp</span>{" "}
-          <span className="font-bold text-green-500">0790931024.</span>
-        </p>
-        <div className="flex text-xl  gap-3">
-          <p>1300/Ukwezi</p>
-          <p className="line-through text-red-500">2000/Ukwezi</p>
-        </div>
-        {currentUser.user.paid ? (
-          <p className="text-green-500 text-3xl font-bold flex justify-center items-center gap-3  ">
-            Paid <RiCheckDoubleFill />
+    <section className="min-h-screen  ">
+      <h2 className="text-3xl font-bold text-center mt-5">
+        Tora uburyo ushaka kwishyuramo
+      </h2>
+      <div className="flex gap-3 flex-wrap m-auto mt-5  justify-center">
+        <div className="pay-card p-3 rounded-lg w-[300px] flex flex-col gap-5 ">
+          <h3 className="text-3xl font-bold">Uburyo 1,</h3>
+          <p>
+            Kanda ahakurikira maze w'ishyure, twashyize ku{" "}
+            <span className="font-bold text-green-500">1300 Rwf.</span> Ufite
+            ikibazo twandikire kuri{" "}
+            <span className="font-bold text-green-500">WhatsApp</span>{" "}
+            <span className="font-bold text-green-500">0790931024.</span>
           </p>
-        ) : (
-          <FlutterWaveButton {...fwConfig} className="btn" />
-        )}
-        {showModal && (
-          <div
-            role="alert"
-            className={`alert ${error ? "alert-error" : "alert-success"}`}
-          >
-            {error ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 shrink-0 stroke-current"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 shrink-0 stroke-current"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            )}
-            <span>{error ? error : "Payment Successful"}</span>
+          <div className="flex text-xl  gap-3">
+            <p>1300/Ukwezi</p>
+            <p className="line-through text-red-500">2000/Ukwezi</p>
           </div>
-        )}
+          {currentUser.user.paid ? (
+            <p className="text-green-500 text-3xl font-bold flex justify-center items-center gap-3  ">
+              Paid <RiCheckDoubleFill />
+            </p>
+          ) : (
+            <FlutterWaveButton {...fwConfig} className="btn" />
+          )}
+          {showModal && (
+            <div
+              role="alert"
+              className={`alert ${error ? "alert-error" : "alert-success"}`}
+            >
+              {error ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 shrink-0 stroke-current"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 shrink-0 stroke-current"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              )}
+              <span>{error ? error : "Payment Successful"}</span>
+            </div>
+          )}
+        </div>
+        <div className="pay-card p-3 rounded-lg w-[300px] flex flex-col gap-5 ">
+          <h3 className="text-3xl font-bold">Uburyo 2,</h3>
+          <p>
+            Wishyure kuri iyi code maze utwandikire kuri WhatsApp : 0790931024
+            utumenyeshe, maze tugushyire muri sisiteme yabishyuye.
+          </p>
+          <div className="flex text-xl  gap-3">
+            <p>1300/Ukwezi</p>
+            <p className="line-through text-red-500">2000/Ukwezi</p>
+          </div>
+        </div>
       </div>
     </section>
   );
