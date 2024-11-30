4225f712d7478fc883610c85141ca13c0d4676e2
biooids <biooidsbiooid@gmail.com>
2024-11-30 23:07:14 +0200

    "Refactor imports and static file serving in server/index.js"

--- server/index.js
+++ server/index.js
@@ -8,18 +8,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const path_1 = __importDefault(require("path")); // Import path in CommonJS style
 dotenv_1.default.config();
 const mongoose_1 = __importDefault(require("mongoose"));
 const auth_routes_1 = __importDefault(require("./routes/auth.routes"));
 const payment_routes_1 = __importDefault(require("./routes/payment.routes"));
+
+const path = require("path");
+
 const app = (0, express_1.default)();
 const PORT = 3000;
-
 if (!process.env.MONGO) {
   throw new Error("MONGO environment variable is missing.");
 }
-
 mongoose_1.default
   .connect(process.env.MONGO)
   .then(() => {
@@ -31,24 +31,17 @@ mongoose_1.default
       error.message || error
     );
   });
-
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use("/api/auth", auth_routes_1.default);
 app.use("/api/payment", payment_routes_1.default);
 
-// Serve the frontend from the "frontend/dist" folder
-app.use(
-  express_1.default.static(path_1.default.join(__dirname, "/frontend/dist"))
-);
+app.use(express_1.default.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(
-    path_1.default.join(__dirname, "frontend", "dist", "index.html")
-  );
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-// Error handling middleware
 app.use((error, req, res, next) => {
   const statusCode = error.status || 500;
   const message =
@@ -56,8 +49,6 @@ app.use((error, req, res, next) => {
     "Iki gikorwa cyanze bitewe nuko: internal server error";
   res.status(statusCode).json({ success: false, statusCode, message });
 });
-
-// Start the server
 const server = app.listen(PORT, () => {
   const addressInfo = server.address();
   if (addressInfo && typeof addressInfo === "object") {
